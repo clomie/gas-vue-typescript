b9861862aa2704c82be1377120ea75a6b0a729c0
clomie <cclomie@gmail.com>
2018-11-05 18:15:28 +0900

    Add .prettierrc file, apply formatting

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,13 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Vue from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default Vue.extend({
-  name: "home",
+  name: 'home',
   components: {
     HelloWorld
   }
-});
+})
 </script>
