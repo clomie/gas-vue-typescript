94b1342383af01b5285b5184f1e5d6526db4f561
clomie <cclomie@gmail.com>
2020-06-14 21:00:48 +0900

    Setup ESLint, Prettier

---
+++ script/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  parserOptions: {},
+}
